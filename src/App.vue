13baffcaf58730607638a290500369bb5ad9c095
Liberontissauri <jp.liberon@gmail.com>
2021-04-01 23:38:13 +0100

    calculator display computed

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,49 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <CalcView :content="calc_view_string" />
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import CalcView from "./components/CalcView.vue";
 
 export default {
   name: "App",
   components: {
-    HelloWorld,
+    CalcView,
   },
+  data() {
+    return {
+      calc_view: ["+1", "(", "-5", "(", "+5", "/", "-5", ")", ")", "+5"],
+      calc_stack: [],
+    };
+  },
+  computed: {
+    calc_view_string: function () {
+      let final_string = "";
+      let show_signal = false;
+      for (let index = 0; index < this.calc_view.length; index++) {
+        const operation = this.calc_view[index];
+        if (operation.includes("*") || operation.includes("/")) {
+          show_signal = false;
+          final_string += operation + " ";
+        } else if (operation.includes("(")) {
+          show_signal = false;
+          final_string += " " + operation;
+        } else if (operation.includes(")")) {
+          final_string += operation + " ";
+        } else if (show_signal == false && !operation.includes("-")) {
+          final_string += operation.slice(1) + " ";
+          show_signal = true;
+        } else {
+          final_string +=
+            operation.slice(0, 1) + " " + operation.slice(1, 2) + " ";
+
+          show_signal = true;
+        }
+      }
+      return final_string;
+    },
+  },
+  methods: {},
 };
 </script>
 
---
+++ src/components/CalcView.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="CalcView-Div">
+      <h2 class="CalcView-Content">{{ content }}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "CalcDisplay",
+  props: {
+    content: String,
+  },
+};
+</script>
+
+<style>
+.CalcView-Div {
+  display: flex;
+  flex-direction: row-reverse;
+}
+.CalcView-Content {
+  margin-right: 40px;
+
+  color: rgb(47, 47, 47);
+  font-size: 24pt;
+}
+</style>
